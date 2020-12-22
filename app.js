d8db0a475cfb458e129ef4bda31274b27ae25936
Utkarsh Patiyal <utkarshpatiyal99@gmail.com>
2020-12-22 12:03:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// db
+mongoose
+    .connect(
+        process.env.MONGO_URI,
+        { useNewUrlParser: true,useUnifiedTopology: true }
+    )
+    .then(() => console.log("DB Connected"));
+
+mongoose.connection.on("error", err => {
+    console.log(`DB connection error: ${err.message}`);
+});
+
+// bring in routes
+const postRoutes = require("./routes/post");
+const authRoutes = require("./routes/auth");
+
+// middleware
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use("/", postRoutes);
+app.use("/", authRoutes);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`A Node Js API is listening on port: ${port}`);
+});
