3aa57f1edcaf5a42953ec75113e3836b38ad6ea9
Utkarsh Patiyal <utkarshpatiyal99@gmail.com>
2020-12-23 19:54:03 +0530

    Create post with image upload

--- controllers/post.js
+++ controllers/post.js
@@ -1,4 +1,6 @@
 const Post=require("../models/post");
+const formidable=require('formidable');
+const fs=require('fs');
 
 exports.getPosts=(req,res)=>{
 	// res.json({
@@ -12,24 +14,33 @@ exports.getPosts=(req,res)=>{
 	.catch(err => console.log(err))
 };
 
-exports.createPost=(req,res)=>{
-	const post=new Post(req.body);
-	// console.log("CREATING POST: ",req.body);
+exports.createPost=(req,res,next)=>{
 
-	// post.save((err,result)=>{
-	// 	if(err){
-	// 		return res.status(400).json({
-	// 			error: err
-	// 		});
-	// 	}
-	// 	res.status(200).json({
-	// 		post: result
-	// 	});
-	// });
+	let form=new formidable.IncomingForm();
+	form.keepExtensions=true;
+	form.parse(req,(err,fields,files)=>{
+		if(err){
+			return res.status(400).json({
+				error:"Image could not be uploaded."
+			})
+		}
+		let post=new Post(fields);
 
-	post.save().then(result =>{
-		res.status(200).json({
-			post: result
+		req.profile.hashed_password=undefined;
+		req.profile.salt=undefined;
+		post.postedBy=req.profile;
+		if(files.photo){
+			post.photo.data=fs.readFileSync(files.photo.path);
+			post.photo.contentType=files.photo.type;
+		}
+		post.save((err,result)=>{
+			if(err){
+				return res.status(400).json({
+					error: err
+				});
+			}
+			res.json(result)
 		});
 	});
+
 };
--- routes/post.js
+++ routes/post.js
@@ -7,7 +7,7 @@ const { userById } = require("../controllers/user");
 const router=express.Router();
 
 router.get('/', getPosts);
-router.post('/post', requireSignin , createPostValidator , createPost);
+router.post('/post/new/:userId', requireSignin , createPost , createPostValidator);//position of createPost is imp
 
 //any route containing :userId, our app will first execute userById() method
 router.param("userId",userById);
