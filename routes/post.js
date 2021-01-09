da3c8d8c9b21bf4b1fbb5aa9a9ba96d35bf70348
Utkarsh Patiyal <utkarshpatiyal99@gmail.com>
2021-01-09 19:13:09 +0530

    Update and Delete Post V-69

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,5 @@
 const express=require('express');
-const {createPost , getPosts,postByUser }=require('../controllers/post');
+const {createPost , getPosts,postsByUser,postById,isPoster,deletePost,updatePost }=require('../controllers/post');
 const { requireSignin } = require("../controllers/auth");
 const {createPostValidator}=require('../validator');
 const { userById } = require("../controllers/user");
@@ -8,8 +8,13 @@ const router=express.Router();
 
 router.get('/', getPosts);
 router.post('/post/new/:userId', requireSignin , createPost , createPostValidator);//position of createPost is imp
-router.get('/posts/by/:userId', requireSignin ,postByUser)
+router.get('/post/by/:userId', requireSignin , postsByUser); 
+router.put("/post/:postId", requireSignin, isPoster, updatePost);
+router.delete("/post/:postId", requireSignin, isPoster, deletePost);
+
 //any route containing :userId, our app will first execute userById() method
 router.param("userId",userById);
+//any route containing :postId, our app will first execute postById() method
+router.param("postId",postById);
 
 module.exports=router;
